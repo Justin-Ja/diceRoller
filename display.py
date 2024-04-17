ead678a9530d0575cdd05aca664549a8d16f3249
Justin J <yustinyakel100@gmail.com>
2024-04-17 17:59:55 -0400

    Added main and display files

---
+++ display.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+def test():
+
+    window = tk.Tk()
+    greeting = tk.Label(text="Hellowordl")
+    greeting.pack()
+
+    window.mainloop()
+
+if __name__ == "__main__":
+    test()
+    
---
+++ main.py
@@ -0,0 +1,10 @@
+import RNG
+
+def main():
+    print("Main file execution goes here")
+    print('launch exe window or something')
+    
+    return 0
+
+if __name__ == "__main__":
+    main()
