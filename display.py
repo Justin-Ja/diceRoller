e59f4c7c0924bdb83cedb0e23ea177ad3f2f6a3a
Justin J <yustinyakel100@gmail.com>
2024-05-01 19:53:14 -0400

    Updated the UI to look better

--- display.py
+++ display.py
@@ -7,6 +7,9 @@ EXTERNAL_FRAME_PADDING: Final[int] = 5
 BUTTON_PADX: Final[int] = 5
 BUTTON_PADY: Final[int] = 10
 
+TITLE_BLUE: Final[str] = "#4B9AFF"
+LIGHT_GRAY: Final[str] = "#CCCCCC"
+
 DICE_SIDES: Final = {
     "d4": 4,
     "d6": 6,
@@ -24,45 +27,45 @@ def launchGUI():
     #TODO: make the app look nice
 
     #Creation of all the frames
-    frameHeader = tk.Frame(master=window, relief=tk.RAISED, width=100, height=75, bg="red")
+    frameHeader = tk.Frame(master=window, relief=tk.RAISED, width=100, height=75, bg=TITLE_BLUE)
     frameHeader.grid(row=0, column=0, sticky="ew", columnspan=3, padx=EXTERNAL_FRAME_PADDING, pady=EXTERNAL_FRAME_PADDING)
 
-    frameButtons = tk.Frame(master=window, bg="lime")
-    frameButtons.grid(row=1, column=0, sticky="nsw", pady=EXTERNAL_FRAME_PADDING, padx=EXTERNAL_FRAME_PADDING)
+    frameButtons = tk.Frame(master=window, bg=LIGHT_GRAY)
+    frameButtons.grid(row=1, column=0, sticky="nsw", pady=EXTERNAL_FRAME_PADDING)
 
-    frameRollResult = tk.Frame(master=window, bg="blue")
+    frameRollResult = tk.Frame(master=window, bg=LIGHT_GRAY)
     frameRollResult.grid(row=1, column=2, sticky="nesw", padx=EXTERNAL_FRAME_PADDING, pady=EXTERNAL_FRAME_PADDING)
 
-    frameRollDice = tk.Frame(master=window, bg="yellow")
-    frameRollDice.grid(row=1, column=1, sticky="nsw", padx=EXTERNAL_FRAME_PADDING, pady=EXTERNAL_FRAME_PADDING)
+    frameRollDice = tk.Frame(master=window, bg=LIGHT_GRAY)
+    frameRollDice.grid(row=1, column=1, sticky="nsw", pady=EXTERNAL_FRAME_PADDING)
 
     #Creation of each label
-    labelHeader = tk.Label(master=frameHeader, text="Dice Roller App")
+    labelHeader = tk.Label(master=frameHeader, text="Dice Roller App", bg=TITLE_BLUE, font=("Roboto", 18, "bold"))
     labelHeader.pack(pady=10)
 
-    labelDiceToRoll = tk.Label(master=frameRollDice, text="Dice to roll: 0")
-    labelDiceToRoll.grid(row=1, column=0)
+    labelDiceToRoll = tk.Label(master=frameRollDice, text="Dice to roll: 0", bg=LIGHT_GRAY, font=("Roboto", 12))
+    labelDiceToRoll.grid(row=1, column=0, padx=10, pady=10)
 
-    labelDisplayTotalRoll = tk.Label(master=frameRollResult, text="You rolled: ")
+    labelDisplayTotalRoll = tk.Label(master=frameRollResult, text="You rolled: ", bg=LIGHT_GRAY, font=("Roboto", 12))
     labelDisplayTotalRoll.pack(expand=True)
 
-    labelDisplayAllGeneratedValues = tk.Label(master=frameRollResult, text="Placeholder")
+    labelDisplayAllGeneratedValues = tk.Label(master=frameRollResult, text="", bg=LIGHT_GRAY, font=("Roboto", 12))
     labelDisplayAllGeneratedValues.pack(expand=True)
 
     #Creates and binds each button to an event handler
     idx = 0
     for idx, (text, sides) in enumerate(DICE_SIDES.items(), start=1):
-        button = tk.Button(master=frameButtons, text=f"Add {text}")
+        button = tk.Button(master=frameButtons, text=f"Add {text}", bd=2, relief=tk.RAISED)
         button.grid(row=idx, column=0, padx=BUTTON_PADX, pady=BUTTON_PADY)
         button.bind("<Button-1>", partial(handleAddDiceToRoll, sides=sides, labelToRoll=labelDiceToRoll, middleware=middlewareInstance))
     
     idx = idx + 1
-    resetButton = tk.Button(master=frameButtons, text="Reset roll")
+    resetButton = tk.Button(master=frameButtons, text="Reset roll", bd=2, relief=tk.RAISED)
     resetButton.grid(row=idx, column=0, padx=BUTTON_PADX, pady=BUTTON_PADY)
     resetButton.bind("<Button-1>", partial(handleClearDiceToRoll, labelToRoll=labelDiceToRoll, middleware=middlewareInstance))
 
-    rollButton = tk.Button(master=frameRollDice, text="Roll dice")
-    rollButton.grid(row=0, column=0, pady=BUTTON_PADY)
+    rollButton = tk.Button(master=frameRollDice, text="Roll dice", bd=2, relief=tk.RAISED)
+    rollButton.grid(row=0, column=0,  sticky="ew", padx=5, pady=BUTTON_PADY)
     rollButton.bind("<Button-1>", partial(handleRollDice, 
                                 labelTotalRolls=labelDisplayTotalRoll, labelAllRolls=labelDisplayAllGeneratedValues, labelToRoll=labelDiceToRoll, middleware=middlewareInstance))
 
@@ -73,7 +76,8 @@ def launchGUI():
 def createWindow():
     window = tk.Tk()
 
-    window.geometry("700x475")
+    window.title("Dice Roller")
+    window.geometry("700x500")
     window.rowconfigure(0, weight=0) 
     window.rowconfigure(1, weight=1) 
     window.columnconfigure(0, weight=0)
